06d651d828307c53228c5273c11bf2cfa9c14d30
KawOwl <kawowl@outlook.com>
2024-02-11 03:05:55 +0800

    add titlebar button function

--- src/component/Titlebar.tsx
+++ src/component/Titlebar.tsx
@@ -1,5 +1,6 @@
 import { RemoveIcon, BackToWindowIcon, FullScreenIcon, ChromeCloseIcon } from '@fluentui/react-icons-mdl2';
 import { Trans } from 'react-i18next';
+import { Window } from '@/backend-command/window';
 
 export const Titlebar = () => {
   return (
@@ -9,16 +10,16 @@ export const Titlebar = () => {
           <Trans>title</Trans>
         </div>
       </div>
-      <div className="titlebar-button" id="titlebar-minimize">
+      <div className="titlebar-button" id="titlebar-minimize" onClick={Window.minimize}>
 				<RemoveIcon />
       </div>
-      <div className="titlebar-button" id="titlebar-restore">
+      <div className="titlebar-button" id="titlebar-restore" onClick={Window.unmaximize} >
 				<BackToWindowIcon />
       </div>
-      <div className="titlebar-button" id="titlebar-maximize">
+      <div className="titlebar-button" id="titlebar-maximize" onClick={Window.maximize} >
 				<FullScreenIcon />
       </div>
-      <div className="titlebar-button" id="titlebar-close">
+      <div className="titlebar-button" id="titlebar-close" onClick={Window.close} >
 				<ChromeCloseIcon />
       </div>
     </div>
